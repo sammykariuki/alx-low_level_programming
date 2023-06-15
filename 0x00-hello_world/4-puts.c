7ab79dd96ca70a938b0a85a2cb4e2d547be990f3
sammykariuki <sammykariuki099@gmail.com>
2023-06-15 14:46:44 -0700

    modified 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
- *main -> Entry point
- *Return -> Always 0 (No error)
+ *main - Entry point
+ *Return: Always 0 (No error)
  */ 
 int main(void)
 {
-	puts("\"Programming is like buikding a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
