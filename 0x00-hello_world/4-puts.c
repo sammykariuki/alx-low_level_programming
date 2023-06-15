92a0e2d42d6c815a1b10fb3b687b28134043fcf3
sammykariuki <sammykariuki099@gmail.com>
2023-06-15 14:41:53 -0700

    created 4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ *main -> Entry point
+ *Return -> Always 0 (No error)
+ */ 
+int main(void)
+{
+	puts("\"Programming is like buikding a multilingual puzzle\n");
+	return 0;
+}
