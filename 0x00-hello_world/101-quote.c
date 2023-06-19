941dae68ce179bd346ee55878e96b9cdfcc8d26c
sammykariuki <sammykariuki099@gmail.com>
2023-06-19 13:16:38 -0700

    created the 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main - The entry point of program
+ *
+ * Return: should be 1 for success
+ */
+int main(void)
+{
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
