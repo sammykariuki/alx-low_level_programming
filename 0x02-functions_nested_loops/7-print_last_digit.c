da594142abc00830385022db40b4c53c807913dd
sammykariuki <sammykariuki099@gmail.com>
2023-06-21 12:48:21 -0700

    created 7-last_digit.c and it's checker and compiled file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - it prints the last digit
+ * gets remainder when number is divided by 10
+ * because it is always same as last digit
+ *
+ * @i: the number being checked
+ *
+ * Return: returns the last digit
+ */
+int print_last_digit(int i)
+{
+	int j;
+
+	j = i % 10;
+	return (j);
+}
