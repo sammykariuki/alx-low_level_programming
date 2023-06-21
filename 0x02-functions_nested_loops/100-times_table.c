e40271186ff6a11d7905d62417e60212a359d067
sammykariuki <sammykariuki099@gmail.com>
2023-06-21 15:33:00 -0700

    created 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_times_table - Prints the table for the input
+ * starting point in 0
+ *
+ * @n: The number of the table to be printed
+ */
+
+void print_times_table(int n)
+{
+	int product, num, m;
+
+	if (n >= 0 && n <= 15)
+	{
+	for (num = 0; num <= n; num++)
+	{
+	_putchar('0');
+	for (m = 1; m <= n; m++)
+	{
+	_putchar(',');
+	_putchar(' ');
+	product = num * m;
+	if (product <= 99)
+	_putchar(' ');
+	if (product <= 9)
+	_putchar(' ');
+	if (product >= 100)
+	{
+	_putchar((product / 100) + '0');
+	_putchar(((product / 10) & 10) + '0');
+	}
+	else if (product <= 99 && product >= 10)
+	{
+	_putchar((product / 10) + '0');
+	}
+	_putchar((product % 10) + '0');
+	}
+	_putchar('\n');
+	}
+	}
+}
