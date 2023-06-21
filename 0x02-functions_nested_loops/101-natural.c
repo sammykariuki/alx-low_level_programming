f8b229e9f5e5371d7b12de68f0c8b7190fd22969
sammykariuki <sammykariuki099@gmail.com>
2023-06-21 15:54:33 -0700

    created 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints sum of multiples
+ * of 3 or 5 up to 1024
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			j += i;
+		}
+		i++;
+	}
+	printf("%d\n", z);
+	return (0);
+}
