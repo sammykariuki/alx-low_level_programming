eb94610b1206aca8da4848868846fde6ae653bda
sammykariuki <sammykariuki099@gmail.com>
2023-06-22 14:14:50 -0700

    created 0-main.c and main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - it tests for negative or positive
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int k;
+	k = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
