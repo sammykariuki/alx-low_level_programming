13229e516ee8b2d3589bdbb22facbf1addbb279d
sammykariuki <sammykariuki099@gmail.com>
2023-06-22 15:20:00 -0700

    updated 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -11,7 +11,6 @@ int main(void)
 	int k;
 
 	k = 0;
-
 	positive_or_negative(k);
 	return (0);
 }
--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,14 +10,13 @@ int main(void)
 {
 	int i;
 
+	printf("Infinite loop incoming :(\n");
 	i = 0;
 
-	printf("Infinite loop incoming \n");
 	/*while (i < 10)*/
 	/*{*/
 	/*putchar(i);*/
 	/*}*/
-
-	printf("Infinite loop avoided!\n");
+	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
