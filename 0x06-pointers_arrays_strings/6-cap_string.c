3b605d0905841192a7bfe24a9952b580a0ea3002
sammykariuki <sammykariuki099@gmail.com>
2023-06-29 13:53:12 -0700

    updated 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,24 +12,31 @@
 
 char *cap_string(char *s)
 {
-	int i, len = 0;
+	int i, len = 0, separator = 1;
 
 	while (*(s + len) != '\0')
 	{
 		len++;
 	}
-	for (i = 0; i < len - 1; i++)
+	for (i = 0; i < len; i++)
 	{
-		if (s[i + 1] >= 97 && s[i + 1] <= 122)
+		if (separator && s[i] >= 97 && s[i] <= 122)
 		{
-		if (s[i] == 32 || s[i] == 9 || s[i] == 10 ||
+			s[i] -= 32;
+		}
+		separator = (s[i] == 32 || s[i] == 9 || s[i] == 10 ||
 		s[i] == 44 || s[i] == 59 || s[i] == 46 ||
 		s[i] == 33 || s[i] == 63 || s[i] == 39 ||
 		s[i] == 40 || s[i] == 41 || s[i] == 123 ||
-		s[i] == 125 || s[i] == 34)
+		s[i] == 125 || s[i] == 34);
+		if (separator && i + 1 < len &&
+		(s[i + 1] == 32 || s[i + 1] == 9 || s[i + 1] == 10 ||
+		 s[i + 1] == 44 || s[i + 1] == 59 || s[i + 1] == 46 ||
+		 s[i + 1] == 33 || s[i + 1] == 63 || s[i + 1] == 39 ||
+		 s[i + 1] == 40 || s[i + 1] == 41 || s[i + 1] == 123 ||
+		 s[i + 1] == 125 || s[i + 1] == 34))
 		{
-			s[i + 1] -= 32;
-		}
+			separator = 1;
 		}
 	}
 	return (s);
