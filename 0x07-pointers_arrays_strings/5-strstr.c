dbdddc665b488b3c8c7839a38a74632d13e8fd17
sammykariuki <sammykariuki099@gmail.com>
2023-07-04 14:30:32 -0700

    created 5-strstr.c that checks for substring in string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring in a string
+ *
+ * @haystack: pointer to the main string
+ * @needle: the substring
+ *
+ * Return: pointer to substring or null
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k = 0, leng = 0;
+
+	while (*(needle + leng) != '\0')
+	{
+		leng++;
+	}
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			k = 1;
+
+			for (j = 1; j < leng; j++)
+			{
+			if (haystack[i + j] == needle[j])
+			{
+				continue;
+			}
+			else
+			{
+				k = 0;
+				break;
+			}
+			}
+		if (k == 1)
+			return (&haystack[i]);
+		}
+	}
+	return (0);
+}
