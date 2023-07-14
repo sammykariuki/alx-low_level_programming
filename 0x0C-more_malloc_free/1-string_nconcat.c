b3447a50d46f23e867bce2919143b63cb839abb4
sammykariuki <sammykariuki099@gmail.com>
2023-07-14 15:43:58 -0700

    add changed '1-string_nconcat.c'

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,62 +2,45 @@
 #include "main.h"
 
 /**
- * _concat - concatenates 2 strings to new allocated
- * location
- * @p: the location pointer
- * @s1: the 1st string pointer
- * @s2: the 2nd string pointer
- * @count1: the length of string 1
- * @count2: the length to be used of string 2
- */
-void _concat(char *p, char *s1, char *s2, int count1, int count2)
-{
-	int i;
-
-	for (i = 0; i < count1; i++)
-	{
-		p[i] = s1[i];
-	}
-	for (i = 0; i < count2; i++)
-	{
-		p[count1 + i] = s2[i];
-	}
-}
-
-/**
- * string_nconcat - concatenates two strings
- * second string only upto n bytes
- * @s1: pointer to first string
- * @s2: pointer to second string
- * @n: the number of bytes of s2
- * Return: pointer to new sting, NULL for fail
+ * *string_nconcat - It concatenates n bytes of a string to another string
+ *
+ * @s1: The string to append to
+ * @s2: The string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
+ *
+ * Return: pointer to the resulting string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	unsigned int count1 = 0, count2 = 0;
+	char *s;
+	unsigned int i = 0, k = 0, count1 = 0, count2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (s2[count2] != '\0')
-		count2++;
-	while (s1[count1] != '\0')
+	while (s1 && s1[count1])
 		count1++;
-	if (n >= count2)
-	{
-		ptr = malloc(sizeof(char) * (count1 + count2 + 1));
-		if (ptr == NULL)
-			return (NULL);
-		_concat(ptr, s1, s2, count1, count2);
-	}
+	while (s2 && s2[count2])
+		count2++;
+
+	if (n < count2)
+		s = malloc(sizeof(char) * (count1 + n + 1));
 	else
+		s = malloc(sizeof(char) * (count1 + count2 + 1));
+
+	if (!s)
+		return (NULL);
+
+	while (i < count1)
 	{
-		ptr = malloc(sizeof(char) * (count1 + n + 1));
-		if (ptr == NULL)
-			return (NULL);
-		_concat(ptr, s1, s2, count1, n);
+		s[i] = s1[i];
+		i++;
 	}
-	return (ptr);
+
+	while (n < count2 && i < (count1 + n))
+		s[i++] = s2[k++];
+
+	while (n >= count2 && i < (count1 + count2))
+		s[i++] = s2[k++];
+
+	s[i] = '\0';
+
+	return (s);
 }
