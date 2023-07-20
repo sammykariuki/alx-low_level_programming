7245ac406ae2c04d81e04399749375da818d9228
Sammy Kariuki <113697588+sammykariuki@users.noreply.github.com>
2023-07-21 00:13:26 +0300

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/1-pi.h
+++
@@ -1,8 +0,0 @@
-#ifndef _1_PI_H_
-#define _1_PI_H_
-
-#ifndef PI
-#define PI 3.14159265359
-#endif
-
-#endif
--- 0x0D-preprocessor/4-sum.h
+++
@@ -1,8 +0,0 @@
-#ifndef _4_SUM_H_
-#define _4_SUM_H_
-
-#ifndef SUM
-#define SUM(X, Y) ((X) + (Y))
-#endif
-
-#endif
